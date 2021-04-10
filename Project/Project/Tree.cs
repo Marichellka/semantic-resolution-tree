946d0e698e544698538c0b45e91a23303fc65bbc
Marichka <marichka.v.richtsi@gmail.com>
2021-04-10 15:27:17 +0300

    Create class Tree
    Add constructor

---
+++ Project/Project/Tree.cs
@@ -0,0 +1,16 @@
+﻿namespace Project
+{
+    public class Tree
+    {
+        public object Key { get; }
+        public object LeftChild { get; private set; }
+        public object RightChild { get; private set; }
+
+        public Tree(object value)
+        {
+            Key = value;
+            LeftChild = null;
+            RightChild = null;
+        }
+    }
+}
