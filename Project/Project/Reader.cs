7351fd0b5ba64af4460ed343d130e9bc92d4bbc0
Marichka <marichka.v.richtsi@gmail.com>
2021-04-10 16:03:13 +0300

    Add constructor

--- Project/Project/Reader.cs
+++ Project/Project/Reader.cs
@@ -1,7 +1,21 @@
-﻿namespace Project
+﻿using System.IO;
+
+namespace Project
 {
     public class Reader
     {
-        
+        private Tree AST;
+
+        public Reader(string path)
+        {
+            string code = "";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                while (!reader.EndOfStream)
+                {
+                    code += reader.ReadLine();
+                }
+            }
+        }
     }
 }
