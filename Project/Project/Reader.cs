921e1b5b59076a86ff810f4325c0cdb689d8992e
Marichka <marichka.v.richtsi@gmail.com>
2021-04-10 16:27:02 +0300

    Add method ParsingString

--- Project/Project/Reader.cs
+++ Project/Project/Reader.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Project
 {
@@ -16,6 +18,35 @@ namespace Project
                     code += reader.ReadLine();
                 }
             }
+        }
+        
+        private List<object> ParsingString(string code)
+        {
+            List<object> tokens = new List<object>();
+            for (int i = 0; i < code.Length; i++)
+            {
+                if (double.TryParse(code[i].ToString(), out double n))
+                {
+                    if (tokens[tokens.Count-1].GetType()==typeof(double))
+                    {
+                        tokens[tokens.Count - 1] = Convert.ToDouble(tokens[tokens.Count - 1].ToString() + code[i]);
+                    }
+                    else
+                    {
+                        tokens.Add(n);
+                    }
+                }
+                else if(code[i]=='.')
+                {
+                    tokens[tokens.Count - 1] = Convert.ToDouble(tokens[tokens.Count - 1].ToString() + code[i]+code[i+1]);
+                    i++;
+                }
+                else
+                {
+                    tokens.Add(code[i]);
+                }
+            }
+            return tokens;
         }
     }
 }
