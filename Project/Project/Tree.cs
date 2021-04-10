cbdb6bc344aac6d63b45c398cdca06bb780306a7
Marichka <marichka.v.richtsi@gmail.com>
2021-04-10 15:51:28 +0300

    Add insertLeft and insertRight methods

--- Project/Project/Tree.cs
+++ Project/Project/Tree.cs
@@ -12,5 +12,29 @@
             LeftChild = null;
             RightChild = null;
         }
+        
+        public void insertLeft(object value)
+        {
+            if(LeftChild==null)
+                this.LeftChild = new Tree(value);
+            else
+            {
+                Tree newNode = new Tree(value);
+                newNode.LeftChild = LeftChild;
+                LeftChild = newNode;
+            }
+        }
+        
+        public void insertRight(object value)
+        {
+            if(RightChild==null)
+                this.RightChild = new Tree(value);
+            else
+            {
+                Tree newNode = new Tree(value);
+                newNode.RightChild = RightChild;
+                RightChild = newNode;
+            }
+        }
     }
 }
