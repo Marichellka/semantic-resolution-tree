56b1b052b02144075aebfc81871fc38e5ec8a3f1
Marichka <marichka.v.richtsi@gmail.com>
2021-04-22 22:27:40 +0300

    Add new constructor

--- Project/Project/Tree.cs
+++ Project/Project/Tree.cs
@@ -6,13 +6,21 @@ namespace Project
     {
         public Tree Parent { get; set; }
         public string Key { get; set; }
-        public List<Tree> Childs { get; } 
+        public List<Tree> Childs { get; set; } 
 
         public Tree(string value)
         {
             Key = value;
             Childs= new List<Tree>();
             Parent = null;
+        }
+        
+        public Tree(string value, Tree leftChild, Tree rightChild)
+        {
+            Key = value;
+            Childs = new List<Tree>();
+            Childs.Add(leftChild);
+            Childs.Add(rightChild);
         }
 
         public void Insert(Tree newNode)
